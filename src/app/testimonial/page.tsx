a872e2312604e964200cccbe7f9d77d7282ded88
isa-ismail <fahimismail75.scc@gmail.com>
2023-10-01 21:58:08 +0600

    some mod

--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -19,12 +19,13 @@ const page = async() => {
         <div className='flex pt-20 justify-center flex-wrap gap-8'>
           {data.slice(0,4).map((item: any) => (
             <div className='flex shadow-xl rounded-lg p-10 flex-col space-y-4 items-center pt-10'>
-              <img className='w-[5rem] h-[5rem] rounded-md' alt='' src={item.image} />
-                  <p className='break-words w-[240px]'>{item.msg}</p>
-                  <p className='text-2xl font-bold'>{item.name}</p>
-                  <p className=''>{ item.designation }</p>
+                <img className='w-[5rem] h-[5rem] rounded-md' alt='' src={item.image} />
+                <p className='break-words w-[240px]'>{item.msg}</p>
+                <p className='text-2xl font-bold'>{item.name}</p>
+                <p className=''>{ item.designation }</p>
             </div>
-          ))}
+          ))
+          }
         </div>
       </div>
     </div>
